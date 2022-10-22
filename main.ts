e4536f8f455cb5a6d536a2d47dceba6216eed2b2
Danilo Just <92367836+jus-dan@users.noreply.github.com>
2022-10-22 17:15:01 +0200

    Aktualisiere pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
 namespace DFPlayerPro {
+    /**
+     * 
+     * 
+     */
     export function testConnection(): string{
+        return "Hello World"
+    }
+
+
+    export function initialize(number: pintx): string {
         return "Hello World"
     }
 }
