5415865ea4a4a8520dbbb4f6b5265ce5ad15a6a6
Danilo Just <92367836+jus-dan@users.noreply.github.com>
2022-10-22 17:32:50 +0200

    serial function added

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
+//% weight=0 color=#b9a0e6 icon="\uf001" block="MP3 Player"
 namespace DFPlayerPro {
+    
+    let MP3_tx = SerialPin.P1
+    let MP3_rx = SerialPin.P2
+
     /**
      * 
      * 
@@ -7,8 +12,20 @@ namespace DFPlayerPro {
         return "Hello World"
     }
 
-
-    export function initialize(): string {
-        return "Hello World"
+    /**
+     * @param pinRX to pinRX ,eg: SerialPin.P2
+     * @param pinTX to pinTX ,eg: SerialPin.P1
+    */
+    //% blockId="DFPlayerPro_setSerial" block="set DFPlayer pro RX to %pinTX| TX to %pinRX"
+    //% weight=100 blockGap=20
+    export function setSerial(pinTX: SerialPin, pinRX: SerialPin): void {
+        MP3_tx = pinTX;
+        MP3_rx = pinRX;
+        serial.redirect(
+            MP3_tx,
+            MP3_rx,
+            BaudRate.BaudRate115200
+        )
+        basic.pause(100)
     }
 }
